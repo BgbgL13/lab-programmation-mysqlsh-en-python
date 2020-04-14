0da7afeeb25963c3f74fe1feaf4977fca6b339c4
Jacques <dorvalfabrice8@gmail.com>
2020-04-14 08:48:29 -0700

    Enlever le commentaire pour afficher le contenu du fichier

--- b000000000.py
+++ b000000000.py
@@ -4,17 +4,33 @@
 @author: 300105201
 """
 
-
-
 import json
 
 def charge(fichier):
    with open(fichier) as f:
       return json.load(f)
+  
+import mysqlx
+
+session = mysqlx.get_session({
+    "host": "localhost",
+    "port": 33060,
+    "user": "root",
+    "password": "password"
+})
+    
+  
+db = session.get_schema("world_x")
 
 def main():
- 
 
+
+# Ne pas oublier de remercier le gestionnaire de BD
+  session.close
+ 
     
-    if __name__== "__main__":
+
+if __name__== "__main__":
       main()
+      
+     
